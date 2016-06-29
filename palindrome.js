b723e065d1c82fa543d9f5dceaaf170f4eaf56a9
Tim Toupin <tim2pin@gmail.com>
2016-06-29 15:47:37 -0500

    palindrome check

---
+++ palindrome.js
@@ -0,0 +1,24 @@
+
+// Write a function isPalindrome(x)
+// that returns true if x is a palindrome,
+// otherwise false.
+//
+// Palindromes are words that are the same
+// going forwards and backwards. Examples:
+//
+// i
+// dod
+// meeteem
+// TrickirT
+
+function isPalindrome(string){
+  return string === string.split('').reverse().join('');
+}
+
+// tests
+
+console.assert( isPalindrome("tacocat") === true )
+console.assert( isPalindrome("Tacocat") === false )
+console.assert( isPalindrome("racecar") === true )
+console.assert( isPalindrome("cowboy") === false )
+
